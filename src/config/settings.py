86f7f59881a65def1c2151439a3bc91c9a33e363
Artem Innokentiev <artinnok@mail.ru>
2017-10-22 19:32:28 +0300

    Change database to PostgreSQL

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
 
 DEBUG = True
 
@@ -53,10 +53,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': 5432,
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
