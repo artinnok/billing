be2bd3f201878f3b1089a2e9712e3182d4fd5ca8
Artem Innokentiev <artinnok@mail.ru>
2017-10-22 21:42:52 +0300

    refactor

--- src/core/forms.py
+++ src/core/forms.py
@@ -5,12 +5,12 @@ from core.models import Profile
 
 class TransactionForm(forms.Form):
     sender = forms.MultipleChoiceField(
-        choices=list(Profile.objects.all().values_list('user', 'inn')),
+        choices=list(Profile.objects.all().values_list('pk', 'inn')),
         label='Отправитель',
     )
     receiver_list = forms.CharField(
         label='Список получателей',
-        help_text='Через запятую'
+        help_text='Через запятую',
     )
     amount = forms.DecimalField(
         max_digits=12,
@@ -19,7 +19,7 @@ class TransactionForm(forms.Form):
         help_text='С точностью до 2 знаков',
     )
 
-    def clean_receiver_list(self, *args, **kwargs):
+    def clean_receiver_list(self):
         receiver_list = self.cleaned_data['receiver_list'].split(',')
 
         return [item for item in receiver_list if item]
