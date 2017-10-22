454d0d678083720d15496f92bfe414936dd346a2
Artem Innokentiev <artinnok@mail.ru>
2017-10-22 19:41:26 +0300

    Added static settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -94,4 +94,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
